009db84198bf471817a8251f3ecab94c521c11f1
luismangubat <joseluismangubat@gmail.com>
2021-03-24 01:42:10 -0400

    React Router Dom
    - Installed React Router Dom
    - Allows for navigation through various web pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './pages/landing';
-
+import { BroweserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
+      <BroweserRouter></BroweserRouter>
 
       <Landing/>
        Hi there
